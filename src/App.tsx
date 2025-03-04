3738637d9bd5b9d496e4262df36ebb3c6a174a0f
Ricardo Mallols <ricardo.mallols@gmail.com>
2025-03-04 20:30:46 +0100

    Add card shuffling mechanism

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import getMessage from './messages';
 import Loading from './loading/Loading';
 import './App.css';
@@ -6,23 +6,53 @@ import './App.css';
 const { text = '', image = '' } = getMessage();
 
 function App() {
-    const [ready, setReady] = useState<boolean>(false);
+
+
+
+    // const [ready, setReady] = useState<boolean>(false);
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setReady(true);
+    //     }, 3000);
+    // }, []);
+
+    // return (
+    //     <div className="App">
+    //         <div className={ready ? 'hide' : 'show'}><Loading /></div>
+    //         <div className={[ready ? 'show' : 'hidden', 'App-content'].join(' ')}>
+    //             <img src={image} alt="Carta Tarot" />
+    //             <h2>{text}</h2>
+    //         </div>
+    //     </div>
+    // );
+
+    const cardList = useRef<HTMLUListElement>(null);
+    const [isAnimating, setIsAnimating] = useState<boolean>(false);
 
     useEffect(() => {
         setTimeout(() => {
-            setReady(true);
-        }, 3000);
+            setIsAnimating(true);
+        }, 1000);        
+        setTimeout(() => {
+            setIsAnimating(false);
+        }, 50000);
     }, []);
 
     return (
-        <div className="App">
-            <div className={ready ? 'hide' : 'show'}><Loading /></div>
-            <div className={[ready ? 'show' : 'hidden', 'App-content'].join(' ')}>
-                <img src={image} alt="Carta Tarot" />
-                <h2>{text}</h2>
-            </div>
+        <div className="card-wrapper">
+            <ul className={['card-list', isAnimating ? 'is-animated' : null].join(' ')} ref={cardList}>
+                <li className="card-list__item" data-card="0">
+                    <div className="card"></div>
+                </li>
+                <li className="card-list__item" data-card="1">
+                    <div className="card"></div>
+                </li>
+                <li className="card-list__item" data-card="2">
+                    <div className="card"></div>
+                </li>
+            </ul>
         </div>
-
     );
 }
 
